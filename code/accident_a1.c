e032c5c0dd8237ff8810cec0ed0dc2ae91f8495d
Joseph Sungpil Choi <chousemath@gmail.com>
2018-12-12 15:49:58 +0900

    Quick save...

--- code/accident_a1.c
+++ code/accident_a1.c
@@ -1,17 +1,65 @@
 #include <assert.h>
+#include <math.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-bool accident_detected(double vel, double accel) {
-  if (vel >= 15 && accel >= 4) {
-    return true;
-  }
-  return false;
+const double POLLING_INTERVAL = 0.3; // in seconds (300ms)
+const double KMPH_TO_KMPS = 0.000277778;
+const double KMPS2_TO_G = 101.971621;
+
+struct DataOBD
+{
+    double Vel;     // 차속 (Km/h) now
+    double VelPrev; // 차속 (Km/h), 300ms before now
+};
+
+struct DataOther
+{
+    double Sound; // in-car sound pressure in dB
+};
+
+// takes two velocities (km/h) and returns the magnitude of the
+// difference in km/s
+double velocityChange(double v1, double v2)
+{
+    return fabs(KMPH_TO_KMPS * (v1 - v2));
 }
 
-int main() {
-  assert(accident_detected(15, 4));
-  assert(accident_detected(16, 5));
-  assert(!accident_detected(13, 4));
-  assert(!accident_detected(15, 3));
-  return 0;
+double getAcceleration(double v1, double v2)
+{
+    return KMPS2_TO_G * (velocityChange(v1, v2) / POLLING_INTERVAL);
+}
+
+bool accidentDetected(struct DataOBD *obd, struct DataOther *other)
+{
+    if (other->Sound >= 160)
+    {
+        return true;
+    }
+    const double vel = fabs(obd->VelPrev);
+    const double accel = getAcceleration(obd->Vel, obd->VelPrev);
+    if (vel > 0.0 && accel >= 4.0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    struct DataOBD dataOBD1;
+    dataOBD1.Vel = 1.5;
+    dataOBD1.VelPrev = 150.56;
+    struct DataOther dataOther1;
+    dataOther1.Sound = 65.5;
+
+    assert(accidentDetected(&dataOBD1, &dataOther1));
+    dataOther1.Sound = 165.5;
+    assert(accidentDetected(&dataOBD1, &dataOther1));
+    dataOBD1.VelPrev = 1.56;
+    assert(accidentDetected(&dataOBD1, &dataOther1));
+    dataOther1.Sound = 65.5;
+    assert(!accidentDetected(&dataOBD1, &dataOther1));
+    return 0;
 }
